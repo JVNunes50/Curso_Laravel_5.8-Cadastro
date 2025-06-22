884c110c9efedf0d0be2bd9d6101ed09b19a5a52
JVNunes50 <jvnunes1944@gmail.com>
2025-06-22 20:29:14 -0300

    HTML - Corrigindo o 'active' no Navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -5,13 +5,13 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item active">
+                <li @if($current=="home") class="nav-item active" @else class="nav-item" @endif>
                     <a class="nav-link" aria-current="page" href="/">Home</a>
                 </li>
-                <li class="nav-item">
+                <li @if($current=="produtos") class="nav-item active" @else class="nav-item" @endif>
                     <a class="nav-link" aria-current="page" href="produtos">Produtos</a>
                 </li>
-                <li class="nav-item">
+                <li @if($current=="categorias") class="nav-item active" @else class="nav-item" @endif>
                     <a class="nav-link" aria-current="page" href="categorias">Categorias</a>
                 </li>
             </ul>
